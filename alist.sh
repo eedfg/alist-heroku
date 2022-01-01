4207364e1d8079d7926709a61eff990d32265cdf
eedfg <73601519+eedfg@users.noreply.github.com>
2022-01-01 17:09:12 +0800

    Update alist.sh

--- alist.sh
+++ alist.sh
@@ -3,17 +3,17 @@ mkdir -p /alist/config/
 
 cat >/alist/config/config.json <<EOF
 {
-  "address": "0.0.0.0"
+  "address": 0.0.0.0
   "port": $PORT
   "database": {
-    "type": "$ADATABASE"
-    "user": "$BSQLUSER"
-    "password": "$CSQLPASSWORD"
-    "host": "$DSQLHOST"
+    "type": $ADATABASE
+    "user": $BSQLUSER
+    "password": $CSQLPASSWORD
+    "host": $DSQLHOST
     "port": $ESQLPORT
-    "name": "$FSQLNAME"
-    "table_prefix": "x_"
-    "db_file": "/alist/config/data.db"
+    "name": $FSQLNAME
+    "table_prefix": x_
+    "db_file": /alist/config/data.db
   }
 }
 EOF
