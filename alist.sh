28ebbcea68c85cf8e319447f3489b7b609b5f9a3
eedfg <73601519+eedfg@users.noreply.github.com>
2022-01-01 17:27:28 +0800

    Update alist.sh

--- alist.sh
+++ alist.sh
@@ -1,64 +1,22 @@
+# 创建配置文件目录
 mkdir -p /alist/config/
 
-# 写入配置文件
-cat >/alist/config/conf.yml <<EOF
-  logo:
-  footer_text: Cooluc's Blog
-  footer_url: https://www.cooluc.com/
-  music_img: https://cdn.cooluc.com/m.png
-  check_update: false
-  script: ""
-  autoplay: true
-  preview:
-    url: ""
-    pre_process: []
-    extensions: []
-    text:
-    - txt
-    - htm
-    - html
-    - xml
-    - java
-    - properties
-    - sql
-    - js
-    - md
-    - json
-    - conf
-    - ini
-    - vue
-    - php
-    - py
-    - bat
-    - gitignore
-    - yml
-    - go
-    - sh
-    - c
-    - cpp
-    - h
-    - hpp
-    max_size: 0
-server:
-  address: 0.0.0.0
-  port: "5244"
-  search: true
-  static: dist
-  site_url: '*'
-  password:
-ali_drive:
-  api_url: https://api.aliyundrive.com/v2
-  max_files_count: 200
-  drives:
-  - refresh_token: $TOKEN
-    root_folder:
-    name: home
-    password: ""
-    hide: false
-database:
-  type: sqlite3
-  dBFile: alist.db
+cat >/alist/config/config.json <<EOF
+{
+  "address": "0.0.0.0",
+  "port": $PORT,
+  "database": {
+    "type": "$ADATABASE",
+    "user": "$BSQLUSER",
+    "password": "$CSQLPASSWORD",
+    "host": "$DSQLHOST",
+    "port": $ESQLPORT,
+    "name": "$FSQLNAME",
+    "table_prefix": "x_",
+    "db_file": "/alist/config/data.db"
+  }
+}
 EOF
 
 cd /alist
-./alist -conf config/conf.yml
+./alist -conf config/config.json
